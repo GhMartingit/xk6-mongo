346bf9f83b85ee0653cc85017ac39745f96bf17f
Harrison Goscenski <hgoscenski@vailsys.com>
2023-10-02 11:20:50 -0500

    tweaking with limit

--- mongo.go
+++ mongo.go
@@ -3,6 +3,7 @@ package xk6_mongo
 import (
 	"context"
 	"log"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -61,12 +62,12 @@ func (c *Client) InsertMany(database string, collection string, docs []any) erro
 	return nil
 }
 
-
-func (c *Client) Find(database string, collection string, filter interface{}) []bson.M{
+func (c *Client) Find(database string, collection string, filter interface{}, limit int64) []bson.M{
 	db := c.client.Database(database)
 	col := db.Collection(collection)
 	log.Print(filter_is, filter)
-	cur, err := col.Find(context.TODO(), filter)
+
+	cur, err := col.Find(context.TODO(), filter, options.Find().SetLimit(limit))
 	if err != nil {
 		log.Fatal(err)
 	}
