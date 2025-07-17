eddd06794accb64bb98997446bb588010f67ddf1
GhMartingit <42034349+GhMartingit@users.noreply.github.com>
2025-07-17 21:45:23 +0400

    docs: note filter type update and add CRUD test

---
+++ crud_test.go
@@ -0,0 +1,62 @@
+package xk6_mongo
+
+import (
+	"os"
+	"testing"
+
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func TestCRUDOperations(t *testing.T) {
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		t.Skip("MONGODB_URI not set")
+	}
+
+	client := new(Mongo).NewClient(uri)
+	if client == nil {
+		t.Fatalf("failed to create client")
+	}
+	defer client.Disconnect()
+
+	db := "crudtestdb"
+	col := "crudtestcol"
+	filter := bson.M{"_id": bson.M{"$eq": "crud-1"}}
+
+	if err := client.Insert(db, col, bson.M{"_id": "crud-1", "name": "init"}); err != nil {
+		t.Fatalf("insert: %v", err)
+	}
+
+	doc, err := client.FindOne(db, col, filter)
+	if err != nil {
+		t.Fatalf("find after insert: %v", err)
+	}
+	if doc["name"] != "init" {
+		t.Fatalf("unexpected name %v", doc["name"])
+	}
+
+	update := bson.D{{"$set", bson.D{{"name", "updated"}}}}
+	if err := client.UpdateOne(db, col, filter, update); err != nil {
+		t.Fatalf("update: %v", err)
+	}
+
+	doc, err = client.FindOne(db, col, filter)
+	if err != nil {
+		t.Fatalf("find after update: %v", err)
+	}
+	if doc["name"] != "updated" {
+		t.Fatalf("unexpected name after update %v", doc["name"])
+	}
+
+	if err := client.DeleteOne(db, col, filter); err != nil {
+		t.Fatalf("delete: %v", err)
+	}
+
+	count, err := client.CountDocuments(db, col, filter)
+	if err != nil {
+		t.Fatalf("count after delete: %v", err)
+	}
+	if count != 0 {
+		t.Fatalf("expected 0 documents, got %d", count)
+	}
+}
--- examples/test-complex-find.js
+++ examples/test-complex-find.js
@@ -3,10 +3,14 @@ import xk6_mongo from 'k6/x/mongo';
 const client = xk6_mongo.newClient('mongodb://localhost:27017');
 
 export default () => {
-  let result, error = client.findOne("testdb", "testcollection", {correlationId: `test--couchbase`});
+  let result, error = client.findOne(
+    "testdb",
+    "testcollection",
+    { score: { "$gte": 10 } }
+  );
   if (error) {
     console.log(error.message);
   } else {
     console.log(result);
   }
-}
+};
